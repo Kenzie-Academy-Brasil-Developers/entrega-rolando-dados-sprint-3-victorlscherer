7df47c64b820de901905d16e3484c1e318d3dee0
Victor Scherer <victor_scherer@hotmail.com>
2021-07-13 16:43:17 -0300

    Trabalho feito

---
+++ dice-roller.js
@@ -0,0 +1,38 @@
+// Simular a rolagem de dois dados várias vezes
+let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+function rollDices() {
+
+    for (let i = 0; i < 1000; i++) {
+        let dice1 = Math.floor(Math.random() * 6 +1);
+        let dice2 = Math.floor(Math.random() * 6 + 1);
+        let sumOfDices = dice1 + dice2;
+        count[sumOfDices - 2] = count[sumOfDices - 2] + 1;
+    }
+}
+rollDices();
+console.log(count);
+
+const backColors = ["Red", "Orange", "Yellow", "Green", "aquamarine","Blue", "Purple", "black", "pink", "brown", "violet"];
+
+const geral = document.getElementsByTagName("body")[0];
+
+function criarBarras() {
+    for (let i = 0; i < count.length; i++) {
+    const faceTitle = document.createElement("h4");
+    faceTitle.innerText = `Resultado da soma ${i + 2}`
+    const resultText = document.createElement("p");
+    resultText.innerText = `${count[i]}`
+    const criarDiv = document.createElement("div");
+    // criarDiv.id = "minhaId";
+    criarDiv.style.width = `${count[i]}px`;
+    criarDiv.style.backgroundColor = `${backColors[i]}`;
+    criarDiv.style.height = "20px";
+
+    geral.appendChild(faceTitle);
+    geral.appendChild(resultText);
+    geral.appendChild(criarDiv);}
+
+}
+
+criarBarras();
